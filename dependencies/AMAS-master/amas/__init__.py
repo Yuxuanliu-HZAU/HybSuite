9bbf3b0e4d2e1d15480e977cfa8805dd872cffa7
Yuxuan Liu <146070633+Yuxuanliu-HZAU@users.noreply.github.com>
2025-03-10 17:25:14 +0800

    Delete dependencies directory

--- dependencies/AMAS-master/amas/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-
-__author__ = 'Marek Borowiec'
-__email__ = 'petiolus@gmail.com'
-__version__ = '1.02'
-__all__ = dir()
--- dependencies/Exe.sh
+++
@@ -1,8 +0,0 @@
-script_dir2="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-if [ ! -s "${script_dir2}/BMGE-main/src/BMGE.jar" ]; then
-cd ${script_dir2}/BMGE-main/src
-javac BMGE.java
-echo Main-Class: BMGE > MANIFEST.MF
-jar -cmvf MANIFEST.MF BMGE.jar BMGE.class bmge/*.class
-rm MANIFEST.MF BMGE.class bmge/*.class
-fi
